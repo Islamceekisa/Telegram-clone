51d5d5b44d6450ce769682198db3d2c881d3e786
Islom <islomnurillayev663@gmail.com>
2021-07-29 17:17:58 +0300

    sliver app

--- lib/Contacts.dart
+++ lib/Contacts.dart
@@ -11,24 +11,44 @@ class _ContactsState extends State<Contacts> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        primary: true,
-        slivers: <Widget>[
-          SliverAppBar(
-            title: Text('Contacts'),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.add),
-                tooltip: 'add',
-                onPressed: () {
-                  // handle the press
-                },
-              ),
-            ],
+        body: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          expandedHeight: 200.0,
+          floating: false,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(background: ProfileCard()),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [SliverListDetail()],
           ),
-          // ...rest of body...
-        ],
-      ),
+        ),
+        SliverFillRemaining(child: LiveTrading(crypto))
+      ],
+    ));
+  }
+
+  Widget SliverListDetail() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              child: Text('LIVE TRADING'),
+            ),
+            Container(
+              child: ElevatedButton(
+                child: Text(
+                  'Buy',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            )
+          ]),
     );
   }
 }
