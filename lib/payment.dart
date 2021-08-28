139541ff8cafeafd66a71e5fd85f1e33122abb7c
Rustam <rustamusmanov708@gmail.com>
2021-08-29 02:12:00 +0500

    payment card added

--- lib/payment.dart
+++ lib/payment.dart
@@ -2,7 +2,18 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class Payment extends StatelessWidget {
+class Payment extends StatefulWidget {
+  @override
+  _PaymentState createState() => _PaymentState();
+}
+
+class _PaymentState extends State<Payment> {
+  int imageIndex;
+
+  List<String> cardImages = [
+    "https://uzoplata.com/wp-content/uploads/2017/01/MObiuzums.png"
+  ];
+
   Widget get backgroundImage => Image.network(
         "https://images.unsplash.com/photo-1550147760-44c9966d6bc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80",
         fit: BoxFit.cover,
@@ -13,6 +24,26 @@ class Payment extends StatelessWidget {
   Widget headText({String text, double size}) => Text(
         text,
         style: TextStyle(fontSize: size, color: Colors.white),
+      );
+
+  Widget paymentCard(String text, IconData icon) => Expanded(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Icon(icon, color: Colors.teal[300]),
+                SizedBox(width: 24),
+                Text(text, style: TextStyle(fontSize: 14, color: Colors.white)),
+              ],
+            ),
+          ),
+          height: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey[850],
+          ),
+        ),
       );
 
   Widget circleItems({String text, IconData icon}) => Column(
@@ -27,6 +58,27 @@ class Payment extends StatelessWidget {
             child: Icon(icon),
           ),
           headText(text: text)
+        ],
+      );
+
+  Widget get transwerPaymentRow => Row(
+        children: [
+          paymentCard("Pul o'tkazish", Icons.present_to_all),
+          SizedBox(width: 20),
+          paymentCard("Pul so'rash", Icons.download),
+        ],
+      );
+
+  Widget titleText(String text) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(text, style: TextStyle(color: Colors.white, fontSize: 16)),
+          Text("YANA",
+              style: TextStyle(
+                color: Colors.teal[400],
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              )),
         ],
       );
 
@@ -47,65 +99,52 @@ class Payment extends StatelessWidget {
               style: TextStyle(color: Colors.white, fontSize: 16),
             ),
             SizedBox(height: 16),
-            Row(
-              children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Text(
-                          "pul o'tkazosh",
-                          style: TextStyle(fontSize: 14, color: Colors.white),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey[850],
-                        ),
-                        height: 70,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 20),
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey[850],
-                    ),
-                    height: 70,
-                  ),
-                ),
-              ],
-            ),
+            transwerPaymentRow,
             SizedBox(height: 20),
-            listOfProviders(subtitle: "text", title: "1000", url: ""),
+            titleText('Saqlangan to\'lovlar'),
+            SizedBox(height: 20),
+            listOfProviders(subtitle: "text", hasImage: true),
+            SizedBox(height: 20),
+            titleText('Saqlangan to\'lovlar'),
+            SizedBox(height: 20),
+            listOfProviders(
+              subtitle: "text",
+              isPaymentCard: true,
+            ),
           ],
         ),
       );
 
-  Widget listOfProviders({String url, String title, String subtitle}) =>
+  Widget listOfProviders({
+    String subtitle,
+    Color bgColor,
+    bool isPaymentCard = false,
+    bool hasImage = false,
+    IconData icon,
+  }) =>
       Container(
         height: 110,
         child: ListView.separated(
           scrollDirection: Axis.horizontal,
+          itemCount: 5,
           separatorBuilder: (context, index) => SizedBox(width: 10),
           itemBuilder: (context, index) => Container(
             width: 100,
             height: 100,
             decoration: BoxDecoration(
-              color: Colors.white,
+              color: isPaymentCard ? Colors.grey[850] : Colors.white,
               borderRadius: BorderRadius.circular(10),
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(title),
+                hasImage
+                    ? Image.network(cardImages.first, width: 70, height: 70)
+                    : Icon(icon, color: Colors.white, size: 24),
                 Text(subtitle),
               ],
             ),
           ),
-          itemCount: 5,
         ),
       );
 
